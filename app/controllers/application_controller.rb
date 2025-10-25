0ed2e14304cdfa8c88e92e5e4a70c1d3ae685c2b
DharmaZR <zalavadiyadharma@gmail.com>
2025-10-24 22:04:49 -0400

    Implemented password hashing and storing. Created login page and signin page.
    Created session,users and pages controller. Session helps control the log in page, users helps control the sign in page and pages helps control the pages after loging in. Passwords and emails and stored securely and password are hashed using rubys built in hasher. Furthermore, created alerts in the signin and login page for the following: wrong password/email, password too short, email already used, passwords do not match up.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    # memoize the current user if session[:user_id] exists
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    current_user.present?
+  end
+
+  def require_login
+    redirect_to login_path, alert: "You must log in first" unless logged_in?
+  end
 end
