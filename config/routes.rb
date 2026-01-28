a166b3e2a846894049d84f6f02647e5a8a37af0d
DharmaZR <zalavadiyadharma@gmail.com>
2026-01-28 13:23:40 -0500

    Adding our services page and started working on login as guest

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,8 @@ Rails.application.routes.draw do
   get "/contactus", to: "pages#contactus", as: "contact"
   get "contactus", to: "pages#contactus"
   post "contact_submit", to: "pages#contact_submit"
+    get "services", to: "pages#services"
+post 'guest_login', to: 'sessions#guest_login', as: :guest_session
+
+
 end
