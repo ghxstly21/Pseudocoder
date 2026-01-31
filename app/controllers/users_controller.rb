72ef8f763b7b8f7d052c379268f2b7f92413b436
DharmaZR <zalavadiyadharma@gmail.com>
2026-01-31 18:19:45 -0500

    Added guest signin and made everything look nicer.  Added a file input/text input that connects to the compiler

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,67 @@
 # app/controllers/users_controller.rb
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [ :edit, :update ]
+  before_action :set_user, only: [ :edit, :update ]
+
   def new
     @user = User.new
   end
 
   def create
-  @user = User.new(user_params)
-  if @user.save
-    redirect_to login_path, notice: "Account created successfully! Please log in."
-  else
-    render :new, status: :unprocessable_entity
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to login_path, notice: "Account created successfully! Please log in."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
-end
 
+  def edit
+  end
+
+  def update
+    if update_password?
+      if @user.authenticate(params[:user][:current_password])
+        @user.password = params[:user][:password]
+        @user.password_confirmation = params[:user][:password_confirmation]
+        if @user.save
+          redirect_to home_path, notice: "Password updated successfully!"
+        else
+          @user.errors.add(:password, "could not be updated")
+          render :edit, status: :unprocessable_entity
+        end
+      else
+        @user.errors.add(:current_password, "is incorrect")
+        render :edit, status: :unprocessable_entity
+      end
+    else
+      if @user.update(user_params_profile)
+        redirect_to home_path, notice: "Profile updated successfully!"
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+  end
 
   private
 
+  def set_user
+    @user = current_user
+  end
+
+  def update_password?
+    params[:user][:password].present? || params[:user][:password_confirmation].present?
+  end
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def user_params_profile
+    params.require(:user).permit(:name, :email)
+  end
+
+  def authenticate_user!
+    redirect_to login_path, alert: "You must log in first" unless current_user.present?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
-  root "home#index"
+  root "pages#home"
+ resources :compilations, only: [ :create, :destroy ]
 
-  get "/login", to: "home#index"
+  get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+  post "/logout", to: "sessions#destroy"
+  get "/guest_login", to: "sessions#guest_login"
+  post "/guest_login", to: "sessions#guest_login"
+  get "/signup", to: "users#new", as: "signup"
 
-  resources :users, only: [ :new, :create ]
+  resources :users, only: [ :new, :create, :edit, :update ]
 
   get "/home", to: "pages#home", as: "home"
   get "/aboutus", to: "pages#aboutus", as: "aboutus"
   get "/contactus", to: "pages#contactus", as: "contact"
-  get "contactus", to: "pages#contactus"
   post "contact_submit", to: "pages#contact_submit"
   get "services", to: "pages#services"
-  post "guest_login", to: "sessions#guest_login", as: :guest_session
 end
