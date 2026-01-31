f730002a73b642a3143c417b4fb6d00243e6ea2e
ghxstly21 <migtav28@bergen.org>
2026-01-30 22:17:22 -0500

    Removed STDIN from pseudocoder.rb.

--- bin/pseudocoder.rb
+++ bin/pseudocoder.rb
@@ -20,7 +20,7 @@ puts "Welcome to Pseudocoder!"
 puts "Your source file will be compiled into English-readable pseudocode."
 puts "Java, JavaScript, and Python files are supported."
 print "1. File input\n2. Copy and pasted code input\nChoose an option: "
-input = STDIN.gets.to_s.strip
+input = gets.to_s.strip
 until %w[1 2].include? input
   print "Invalid input. Please reenter your choice: "
   input = gets.to_s.strip
@@ -29,14 +29,14 @@ case input
 when "1"
   print "Please enter a file path: "
 
-  file = normalize_path(STDIN.gets.chomp)
+  file = normalize_path(gets.chomp)
 
   until is_valid? file
     puts file.inspect
     puts "The file you entered could not be found on your computer." unless File.exist?(file)
     puts "The file you entered is not one of our supported languages. (Java, JavaScript, Python)" unless SUPPORTED_LANGUAGES.include?(File.extname(file))
     print "Please reenter the file path: "
-    file = normalize_path(STDIN.gets.chomp)
+    file = normalize_path(gets.chomp)
   end
   puts "File is valid!\nCompiling..."
 
@@ -44,7 +44,7 @@ when "1"
 
 when "2"
   puts "Paste your code below:"
-  code = STDIN.gets.chomp
+  code = gets.chomp
   elapsed = Compiler.time { pseudocode = Compiler.compile_text code }
 else
   raise "Unexpected input: #{input.inspect}.\nExpected '1' or '2'."
