6e4b49bc7d120d03c4387fd5154b0c942e1e14db
ghxstly21 <migtav28@bergen.org>
2026-02-07 19:41:50 -0500

    Added generator functionality for basic js
    Also improved timing of compilation by using BigDecimal

--- app/services/compiler/generator.rb
+++ app/services/compiler/generator.rb
@@ -1,14 +1,28 @@
+require_relative "ast"
+
 module Compiler
 class Generator
   attr_reader :ast
-  def initialize(ast, lang)
+  def initialize(ast)
     @ast = ast
-    @lang = lang
   end
   def generate(node)
+    return "" if node.nil?
     case node
+    when ProgramNode
+      generate(ast.nodes.first)
     when FunctionNode
-
+      <<~FUNCTION
+        function #{node.name}(#{node.arg_names.join(", ")}) do
+            #{generate(node.body)}
+        end
+        FUNCTION
+    when ExpressionNode
+      case node
+      when NumberNode || VarRefNode then "#{node.value}"
+      when CallNode then "#{node.name}(#{node.arg_exprs.join(", ")})"
+      else raise "Currently unsupported expression on type #{node.class} at location #{node.location}.\nAdd a case!"
+      end
     else raise RuntimeError.new("Unexpected node type: #{node.class}")
     end
   end
